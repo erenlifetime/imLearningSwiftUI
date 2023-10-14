8556069cfd1972f77ac6b86a3f136e108dc606aa
erenlifetime <medya_194@hotmail.com>
2023-10-14 17:46:48 +0300

    Initial Commit

---
+++ LetsWriteSwiftUI/LetsWriteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsWriteSwiftUIApp.swift
+//  LetsWriteSwiftUI
+//
+//  Created by Eren lifetime on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LetsWriteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
